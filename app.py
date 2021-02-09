ee8fab5e4e78568331251c5e1a02c3c5599b6d54
Harshit <harshit.2115@gmail.com>
2021-02-09 19:11:06 +0530

    final

--- app.py
+++ app.py
@@ -11,12 +11,13 @@ def home():
 
 @app.route('/predict', methods=['POST'])
 def predict():
-    int_features = [float(x) if (x != "") else np.nan for x in request.form.values()]
+    int_features = [float(x) if (
+        x != "") else np.nan for x in request.form.values()]
     final_features = np.array(int_features).reshape((1, -1))
     model = joblib.load('./model.pkl')
     pred = model.predict(final_features)
-    
+
     return render_template('index.html', predicted_value=f'Predicted Value is {float(pred)}')
-    
+
 if __name__ == "__main__":
     app.run(debug=True)
